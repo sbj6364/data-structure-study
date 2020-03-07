7c043f7660d37ebe1e20dc53328f6598433fbc31
sbj6364 <sbj6364@kakao.com>
2020-03-07 19:05:21 +0900

    Dynamic Memory Allocation into Double Array
    Allocating memories by using double pointer

---
+++ DynamicMemoryAllocation/DynamicMemoryAllocation#3.c
@@ -0,0 +1,27 @@
+﻿#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+	int i;
+	char** pch;
+	pch = (char**)malloc(3 * sizeof(char*));
+	for (i = 0; i < 3; i++)
+		pch[i] = (char*)malloc(4 * sizeof(char));
+
+	strcpy(pch[0], "aaa");
+	strcpy(pch[1], "bbb");
+	strcpy(pch[2], "ccc");
+
+	for (i = 0; i < 3; i++)
+		puts(pch[i]);
+	for (i = 0; i < 3; i++)
+		free(pch[i]);
+
+	free(pch);
+
+	return 0;
+}
