f7d4c5c1be2eaeb701d948a3f834ead28a6db51f
SeoByeongjun <sbj6364@kakao.com>
2020-04-11 18:49:44 +0900

    Create Recursion#6.c

---
+++ Recursion/Recursion#6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int gcd(int x, int y)
+{
+	if (y == 0) return x;
+	else return gcd(y, x % y);
+}
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", gcd(a, b));
+	return 0;
+}
